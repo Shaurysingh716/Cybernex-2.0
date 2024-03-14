a482bcb4d8e6ea8c2f2abd5fc483a1f85b38e5a1
Shaurysingh716 <shaurysingh2134@gmail.com>
2024-03-14 13:51:48 +0530

    Restored Old Code in Comments

---
+++ script.js
@@ -0,0 +1,6 @@
+// function toggleTextVisibility(element) {
+//     element.style.display = "block";
+//     setTimeout(function () {
+//         element.style.display = "none";
+//     }, 5000);
+// }

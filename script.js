208fb13b11e0bbcf0c2f79033a1d61acef3a6055
Shaurysingh716 <shaurysingh2134@gmail.com>
2024-03-14 13:50:40 +0530

    Display Issue Fixed At The Landing Page

--- script.js
+++
@@ -1,6 +0,0 @@
-function displayText() {
-    document.getElementById("textContainer").style.display = "block";
-    setTimeout(function(){
-        document.getElementById("textContainer").style.display = "none";
-    }, 3000);
-}

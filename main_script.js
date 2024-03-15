2e77c2686e2b3a3f2874b11c67e280c44d3e9114
Vansh <vansh.kash05@gmail.com>
2024-03-15 15:20:42 +0530

    index.html bg updated, scripting for file linkage started

--- main_script.js
+++ main_script.js
@@ -4,3 +4,10 @@
 //         element.style.display = "none";
 //     }, 5000);
 // }
+
+var test_1_result=false
+var test_2_result=false
+var test_3_result=false
+var test_4_result=false
+var test_5_result=false
+
